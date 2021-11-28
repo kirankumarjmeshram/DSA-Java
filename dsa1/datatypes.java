31c0f0c50303760427778c7c8c2df93597476f86
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-28 17:12:23 +0530

    basic datatypes

---
+++ dsa1/datatypes.java
@@ -0,0 +1,21 @@
+public class  datatypes{
+    private static void name(String[] args) {
+        
+        // inegers are primitive .. ir cannot break further
+        //string is non primitive
+        //i.e. it can break further ie. kiran = k+i+r+a+n
+        int rollno =64; //till 4 bytes
+        String name = "kiran"; // String starts with double quotes(" ")
+        char letter ='r';
+        float marks = 98.67f;  //4 bytes
+        double largeDecimalNumbers = 9856897.478959; //8bytes
+        long largeInteger = 90265678L; //8bytes
+        boolean check = true;
+
+        //L and f are for large vales
+
+
+
+
+    }
+}
