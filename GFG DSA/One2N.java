4c51ba5a8aff875276bebdc8c83e95dcbf5d2800
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-08-14 19:47:37 +0530

    one to N

---
+++ GFG DSA/One2N.java
@@ -0,0 +1,16 @@
+class One2N
+{
+public static void main(String[] args){
+    int N = 10;
+  printNos(N)
+    {
+        //Your code here
+        if(N>0){
+            printNos(N-1);
+            System.out.print(N+ " ");
+        }
+        return;
+    }
+} 
+
+}
