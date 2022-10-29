bf04a4730c33024df3806f67cd084b63f8ff4db8
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-29 17:11:30 +0530

    java input output

---
+++ HackerRank/javaStdinOut.java
@@ -0,0 +1,18 @@
+package HackerRank;
+import java.util.*;
+
+public class javaStdinOut {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        // Complete this line
+        // Complete this line
+        for(int i=0;i<3;i++){
+            int a = scan.nextInt();
+            System.out.println(a);
+        }
+        scan.close();
+        // Complete this line
+        // Complete this line
+    }
+}
