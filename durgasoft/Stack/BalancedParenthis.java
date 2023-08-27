9cee82b0001220563ba111d1b8a0760c11a4bfd5
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-08-27 11:43:46 +0530

    balance parenthesis

--- durgasoft/Stack/BalancedParenthis.java
+++ durgasoft/Stack/BalancedParenthis.java
@@ -1,3 +1,37 @@
+import java.util.*;
 class Test {
-    
+    static boolean isBalanceParenthis(String s)
+    {
+        Stack <Character> ss = new Stack<Character>();
+        for(char ch:s.toCharArray())
+        {
+            switch(ch)
+            {
+                case '(' :
+                case '[' :
+                case '{' : ss.push(ch); break;
+                case ')' : if(ss.pop()!= '(')
+                                return false;
+                            break;
+                case ']' : if(ss.pop() != '[')
+                                return false;
+                            break;
+                case '}' : if(ss.pop() != '{')
+                                return false;
+                            break;
+            }
+        }
+        return ss.empty();
+    }
+
+    public static void main(String[] args) 
+	{
+		System.out.println(isBalanceParenthis("()"));//true
+		System.out.println(isBalanceParenthis("[]"));//true
+		System.out.println(isBalanceParenthis("{}"));//true
+		System.out.println(isBalanceParenthis("(())"));//true
+		System.out.println(isBalanceParenthis("([})"));//false
+		System.out.println(isBalanceParenthis("([{}])"));//true
+		System.out.println(isBalanceParenthis("([{]})"));//false
+	}
 }
