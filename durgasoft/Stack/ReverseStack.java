5ad7b86f6b671544d06d4f3a79e69065b64293ae
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-08-27 11:21:53 +0530

    reverse stack

--- durgasoft/Stack/ReverseStack.java
+++ durgasoft/Stack/ReverseStack.java
@@ -1,3 +1,40 @@
+import java.util.Stack;
+
 class Test {
-    
+    static void bottomInsert(Stack<Integer> ss, int value)
+	{
+		int temp;
+		if(ss.empty())
+			ss.push(value);
+		else
+		{
+			temp = ss.pop();
+			bottomInsert(ss,value);
+			ss.push(temp);
+		}
+	}
+
+    static void reverseStack(Stack<Integer> ss){
+        if(ss.empty()){
+            return;
+        }else{
+            int temp = ss.pop();
+            reverseStack(ss);
+            bottomInsert(ss, temp);
+        }
+    }
+
+    public static void main(String[] args) 
+	{
+		Stack<Integer> s = new Stack<Integer>();
+
+		s.push(111);
+		s.push(444);
+		s.push(222);
+		s.push(555);
+		s.push(333);
+		System.out.println(s);//[111, 444, 222, 555, 333]
+		reverseStack(s);
+		System.out.println(s);//[333, 555, 222, 444, 111]
+	}
 }
