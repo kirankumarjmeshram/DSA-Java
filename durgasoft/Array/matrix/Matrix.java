9f67703fa172deeede8e651c367beae16f94f908
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-06 18:31:04 +0530

    Reading and Writing Matrix Elements

---
+++ durgasoft/Array/matrix/Matrix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Matrix
+{
+	public static void main(String[] args) 
+	{
+		Scanner obj = new Scanner(System.in);
+		int[][] a = new int[3][3];
+
+		int i,j;
+		System.out.println("Enter matrix elements: ");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				a[i][j] = obj.nextInt();
+			}
+		}
+
+		System.out.println("MATRIX ELEMENTS:");
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<3;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
